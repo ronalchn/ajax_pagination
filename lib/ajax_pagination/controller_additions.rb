2759a803b430046d28a2e4c002290be3a9d60c56
Ronald Chan <ronalchn@gmail.com>
2012-02-19 12:41:18 +1300

    Add rdoc comments to functions

--- lib/ajax_pagination/controller_additions.rb
+++ lib/ajax_pagination/controller_additions.rb
@@ -1,5 +1,30 @@
 module AjaxPagination
+  # This module is automatically added to all controllers
   module ControllerAdditions
+    # Registers a javascript format when params[:pagination] matches options[:pagination] ( = "page" by default).
+    # AJAX Pagination uses this response to render only the content which has changed. When this format is triggered,
+    # a partial is passed back, and sent to AJAX Pagination as a function argument in javascript.
+    #
+    # Call this method in a respond_to block, in a controller action:
+    #
+    #   class CommentsController < ApplicationController
+    #     def index
+    #       @comments = Comment.all
+    #       respond_to do |format|
+    #         format.html # index.html.erb
+    #         ajax_pagination(format)
+    #       end
+    #     end
+    #   end
+    #
+    # Options:
+    # [:+pagination+]
+    #   Changes the pagination name triggering this response. Triggered when +params[:pagination]+ == +options[:pagination]+.
+    #   Defaults to "page"
+    #
+    # [:+partial+]
+    #   Changes the partial that is returned by this response. Defaults to +options[:pagination]+.
+    #
     def ajax_pagination(format,options = {})
       if params[:pagination] == (options[:pagination] || 'page')
         partial = options[:partial] || params[:pagination]
--- lib/ajax_pagination/helper_additions.rb
+++ lib/ajax_pagination/helper_additions.rb
@@ -1,5 +1,50 @@
 module AjaxPagination
+  # This module is automatically added to all views, providing helper methods.
   module HelperAdditions
+    # Renders a partial wrapped in a div tag. When the page content displayed in the partial needs changing
+    # AJAX Pagination will replace content inside this div tag, with the new content returned by the controller
+    # (To manage that, see +ajax_pagination+ in ControllerAdditions).
+    # 
+    # Call this method instead of +render+ to display the partial representing the page content:
+    #
+    #   Listing Comments:
+    #   
+    #   <%= ajax_pagination %>
+    #   
+    #   <%= link_to 'New Comment', new_comment_path %>
+    #
+    # Options:
+    # [:+pagination+]
+    #   Changes the pagination name, which is used for requesting new content, and to uniquely identify the
+    #   wrapping div tag. The name passed here should be the same as the pagination name used in the controller
+    #   respond_to block. Defaults to "page".
+    #
+    # [:+partial+]
+    #   Changes the partial which is rendered. Defaults to +options[:pagination]+. The partial should generally
+    #   be the same as that given in the controller respond_to block, unless you are doing something strange.
+    #
+    # [:+reload+]
+    #   Used to detect whether the partial needs reloading, based on how the url changes. When pagination links are
+    #   clicked, they are easily detected, and will load the new content automatically. When the browser
+    #   back/forwards buttons are used, AJAX Pagination needs to know whether the content inside this pagination
+    #   partial needs reloading. This is not certain, because the webpage may contain multiple pagination partials,
+    #   and moreover, the page may have other functionality using the History.pushState feature. Defaults to nil.
+    #   
+    #   If passed a hash of the form :+query+ => "parametername", AJAX Pagination will parse the url, to find
+    #   the parameter +parametername+ inside the query string (ie. /path/?parametername=value). When it changes,
+    #   the partial is reloaded.
+    #   
+    #   If passed a hash of the form :+urlregex+ => "regularexpression", AJAX Pagination will apply the regular
+    #   expression to the url. If a particular subexpression of the match changes, the partial is reloaded. The
+    #   subexpression used defaults to the whole match. If the hash includes :+regexindex+ => N, the Nth subexpression
+    #   is used.
+    #   
+    #   Different parts of the url can be watched for any changes, by passing an array of hashes. For example:
+    #   
+    #     <%= ajax_pagination :reload => [{:urlregex => "page=([0-9]+)", :regexindex => 1},{:query => "watching"}] %>
+    #   
+    #   If nil, AJAX Pagination acts as if it was passed {:query => options[:pagination]}.
+    #
     def ajax_pagination(options = {})
       pagination = options[:pagination] || 'page' # by default the name of the pagination is 'page'
       partial = options[:partial] || pagination # default partial rendered is the name of the pagination
@@ -15,6 +60,23 @@ module AjaxPagination
         render partial
       end
     end
+
+    # Wraps content in a div tag. AJAX Pagination uses this to display visual loading cues. When a partial
+    # is reloaded, it may take some time. In the meanwhile, AJAX Pagination will look in the partial amoung its
+    # immediate child for this tag. If this tag exists, it will cover this tag with a semi-transparent
+    # rectangle, and make the old partial unclickable (links and other elements are therefore disabled).
+    # A loading image is also displayed above the content.
+    #
+    # Use this tag in your partial, wrapped around all the content you want to disable. For example, if you are
+    # displaying pagination links which you do not want to disable, as well as content you wish to disable,
+    # you partial might contain:
+    #
+    #   <%= will_paginate @objects, :params => { :pagination => nil } %>
+    #   <%= ajax_pagination_loadzone do %>
+    #     All content here is covered by a semi-transparent rectangle.
+    #     A loading image is displayed on top, and any links here are unclickable
+    #   <% end %>
+    #
     def ajax_pagination_loadzone()
       content_tag :div, :class => "paginated_content", :style => "position: relative" do
         yield
