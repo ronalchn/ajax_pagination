5ee46f5637487b8840fe0c888b12cff9a579cc5e
Ronald Chan <ronalchn@gmail.com>
2012-03-09 23:50:19 +1300

    update generator

--- lib/generators/ajax_pagination/assets_generator.rb
+++ lib/generators/ajax_pagination/assets_generator.rb
@@ -7,7 +7,7 @@ if true #::Rails.version < "3.1" || !::Rails.application.config.assets.enabled
   module AjaxPagination
     module Generators
       class AssetsGenerator < Rails::Generators::Base
-        source_root File.expand_path("../../../assets",__FILE__)
+        source_root File.expand_path("../../../../",__FILE__)
 
         desc "Fetches the ajax_pagination assets - javascript asset is processed based on current environment"
 
@@ -16,9 +16,11 @@ if true #::Rails.version < "3.1" || !::Rails.application.config.assets.enabled
           
           File.makedirs("public/javascripts/")
           create_file "public/javascripts/ajax_pagination.js", jstemplate.result(binding)
+          copy_file "vendor/assets/javascripts/jquery.ba-bbq.js", "public/javascripts/jquery.ba-bbq.js"
+          copy_file "vendor/assets/javascripts/jquery.url.js", "public/javascripts/jquery.url.js"
         end
         def css
-          copy_file "stylesheets/ajax_pagination.css", "public/stylesheets/ajax_pagination.css"
+          copy_file "lib/assets/stylesheets/ajax_pagination.css", "public/stylesheets/ajax_pagination.css"
         end
 
         private
